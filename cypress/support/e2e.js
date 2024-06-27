c86a972dffd005287630ee9004b44b549135434d
Carlos Armando Estrada Torres <carlos.estrada@blankfactor.com>
2024-06-27 13:44:28 -0500

    improve security of login users

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import { before } from 'mocha';
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
