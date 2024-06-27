041ca22ab4d86ffe78ccff571c289503a69c29b9
Carlos Armando Estrada Torres <carlos.estrada@blankfactor.com>
2024-06-26 21:24:54 -0500

    First Setup of Cypress proyect

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  'cypress-cucumber-preprocessor': {
+    nonGlobalStepDefinitions: false,
+    step_definitions: './cypress/e2e/step_definitions/**',
+  },
+  numTestsKeptInMemory: 0,
+  defaultCommandTimeout: 42000,
+  pageLoadTimeout: 300000,
+  responseTimeout: 60000,
+  requestTimeout: 405700,
+  chromeWebSecurity: false,
+  e2e: {
+    baseUrl: 'https://www.saucedemo.com',
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.feature',
+    defaultCommandTimeout: 10000,
+    supportFile:false
+  },
+});
